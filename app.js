754e6af4b1bfdf3aebb59a1e94b7f43859ca8fd4
TONY ADAMS <truvisory@TONYs-MacBook-Pro.local>
2019-01-16 11:49:22 -0700

    listening to Ports

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ const port = 3002
 const data = require('./data.json')
 
 app.use(express.static('public'))
+
+app.get('/', (req, res, next) => {
+  res.status(200)
+  res.send(data)
+})
+
+app.get('/:tag', (req, res, next) => {
+  const tag = req.params.tag
+  const songs = data.songs.filter(song => song.tags.includes(tag))
+  data.tags.includes(tag)
+    ? res.send(songs)
+    : res.status(404).send('Tag not Matched')
+})
+
+const listener = () => console.log(`Listening on port ${port}!`)
+app.listen(port, listener)
